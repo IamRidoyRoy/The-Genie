bb2eed8631d8e3255d6598b7b11543508a213f36
DevXChardike <chardikeltd@gmail.com>
2022-11-19 17:46:59 +0600

    Add new game start and Surrender method

--- app.js
+++ app.js
@@ -66,6 +66,9 @@ const app = Vue.createApp({
             this.playerHealth = 100;
             this.winner = null;
             this.currentRound = 0;
+        },
+        surrender() {
+            this.winner = 'monster';
         }
 
     },
